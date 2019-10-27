9f375f75b4f761358652b563b0db4280681b4069
Stephen Martinez <enharmonicnuance@gmail.com>
2019-10-26 18:02:13 -0700

    Initial Commit

---
+++ Sources/Injectable/Injectable.swift
@@ -0,0 +1,81 @@
+//
+//  Injectable.swift
+//  Injectable
+//
+//  MIT License
+//
+//  Copyright (c) 2019 STEPHEN L. MARTINEZ
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+
+import Foundation
+
+struct FatalInjectable {
+    
+    private static let tag = "Injectable Error: "
+    
+    static func serviceNotRegistered<T>(_ :T.Type) -> Never {
+        let errorMessage = "\(T.self) was never registered!"
+        return fatalError(tag + errorMessage)
+    }
+    
+    static func serviceNotProtocol<T>(_ :T.Type) -> Never {
+        let errorMessage = "\(T.self) is not a protocol! Services must be a protocol."
+        return fatalError(tag + errorMessage)
+    }
+    
+}
+
+final class Services {
+    
+    private init() {}
+    
+    static let root = Services()
+    
+    private typealias ServiceBuilder = () -> Any
+    
+    private var builders = [String: ServiceBuilder]()
+    
+    func register<T>(_ serviceBuilder: @escaping () -> T) {
+        
+        guard isProtocol(type: T.self) else {
+            FatalInjectable.serviceNotProtocol(T.self)
+        }
+        
+        builders[key(for: T.self)] = serviceBuilder
+    }
+    
+    func retrieve<T>() -> T {
+        
+        guard isProtocol(type: T.self) else {
+            FatalInjectable.serviceNotProtocol(T.self)
+        }
+        
+        let serviceBuilder = builders[key(for: T.self)]
+        let extractedService = serviceBuilder?() as? T
+        
+        guard let service = extractedService else {
+            FatalInjectable.serviceNotRegistered(T.self)
+        }
+        
+        return service
+    }
+    
+}
---
+++ Sources/Injectable/Interface.swift
@@ -0,0 +1,55 @@
+//
+//  Interface.swift
+//  Injectable
+//
+//  MIT License
+//
+//  Copyright (c) 2019 STEPHEN L. MARTINEZ
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+
+import Foundation
+
+/// A Convenience `protocol` to make the intent explicit that conforming `Type`s will be used as an `InjectableService`
+/// Conformance to this `protocol` is not necessary for registration as an `InjectableService`. However it is necessary to
+/// conform to some protocol. This empty protocol is available as a convenience for that purpose.
+public protocol InjectableService { }
+
+@propertyWrapper
+public struct Inject<T> {
+    
+    public var wrappedValue: T {
+        InjectableServices.retrieve()
+    }
+    
+    public init() {}
+}
+
+public struct InjectableServices {
+    
+    public static func register<T>(_ builder: @autoclosure @escaping () -> T) {
+        Services.root.register(builder)
+    }
+    
+    public static func retrieve<T>() -> T {
+        Services.root.retrieve()
+    }
+    
+}
